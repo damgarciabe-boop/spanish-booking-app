59530fb7db76583e60418007543c842226ac2c84
Linda <s241892@bi.no>
2026-04-29 13:23:32 +0200

    views, urls and templates for students

--- spanishapp/views.py
+++ spanishapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import StudentRegistrationForm
+from .models import LanguageLevel, CourseType, StudentProfile, TeacherProfile, Status, TimeSlot, Booking 
 
 def home(request):
     return render(request, 'spanishapp/home.html')
@@ -42,3 +43,50 @@ def student_dashboard(request):
 @login_required
 def teacher_dashboard(request):
     return render(request, 'spanishapp/teacher_dashboard.html')
+
+@login_required
+def booking (request):
+    courses = CourseType.objects.all()
+    return render(request, "spanishapp/booking.html", {"courses": courses} )
+
+@login_required
+def booking_teachers (request, course_id):
+    course = CourseType.objects.get(id=course_id)
+    teachers= TeacherProfile.objects.filter(courses=course)
+    return render(request, "spanishapp/booking_teachers.html", {"course": course, "teachers": teachers})
+
+@login_required
+def booking_timeslot (request, course_id, teacher_id):
+    course = CourseType.objects.get(id=course_id)
+    teacher= TeacherProfile.objects.get(id=teacher_id)
+    time_slots=TimeSlot.objects.filter(teacher=teacher,is_available=True)
+    return render(request, "spanishapp/booking_timeslot.html", {"course":course, "teacher":teacher, "time_slots":time_slots})
+
+@login_required
+def booking_confirm (request, course_id, teacher_id, timeslot_id):
+    course = CourseType.objects.get(id=course_id)
+    teacher= TeacherProfile.objects.get(id=teacher_id)
+    timeslot=TimeSlot.objects.get(id=timeslot_id)
+    
+    if request.method == "POST":
+        already_booked = Booking.objects.filter(time_slot=timeslot).exists()
+        
+        if already_booked:
+            return redirect("/my_bookings/")
+        
+        student=StudentProfile.objects.get(id=request.user.id)
+        status=Status.objects.get(title="Pending")
+        Booking.objects.create(student=student, course=course, time_slot=timeslot, status=status)
+        
+        timeslot.is_available=False
+        timeslot.save()
+        return redirect("/my_bookings/") 
+    return render(request, "spanishapp/booking_confirm.html", {"course":course, "teacher":teacher, "timeslot":timeslot})
+
+
+@login_required
+def my_bookings (request):
+    student = StudentProfile.objects.get(id=request.user.id)
+    bookings = Booking.objects.filter(student=student)
+    return render(request, "spanishapp/my_bookings.html", {"bookings": bookings})
+
--- spanishproject/urls.py
+++ spanishproject/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from spanishapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +12,12 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('student/dashboard/', views.student_dashboard, name='student_dashboard'),
     path('teacher/dashboard/', views.teacher_dashboard, name='teacher_dashboard'),
+    path('booking/', views.booking, name='booking'),
+    path('booking/teachers/<int:course_id>/', views.booking_teachers, name='booking_teachers'),
+    path('booking/timeslots/<int:course_id>/<int:teacher_id>/', views.booking_timeslot, name='bookingtimeslot'),
+    path('booking/confirm/<int:course_id>/<int:teacher_id>/<int:timeslot_id>/', views.booking_confirm, name='booking_confirm'),
+    path('my_bookings/', views.my_bookings, name='my_bookings'),
+    
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
